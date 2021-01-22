3cd1b000f3d54f5fcc775949bc6ac84902af847d
Rob Deeb <mrrobby@museality.com>
2021-01-21 19:14:08 -0800

    Updates SQL with UTC

--- include/templates/cloud_sql_cdc/export_csv/table_2.sql
+++ include/templates/cloud_sql_cdc/export_csv/table_2.sql
@@ -1,4 +1,4 @@
 {%- set low_watermark =  execution_date.subtract(hours=1) -%}
 SELECT * FROM some_schema.table_2
 WHERE event_timestamp AT TIME ZONE 'UTC' >= '{{ low_watermark }}' AT TIME ZONE 'UTC'
-AND event_timestamp AT TIME ZONE 'UTC' < '{{ execution_date }}'
+AND event_timestamp AT TIME ZONE 'UTC' < '{{ execution_date }}' AT TIME ZONE 'UTC'
